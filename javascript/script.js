272fa1de2867568fd2477756c120f64b90490dd5
patrik <patrikyoussef@gmail.com>
2018-02-06 00:18:44 +0100

    slutversion

---
+++ javascript/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+ 
+var galleriBilder = $(".bildContainer-galeri img");
+var pos = 0;
+
+function galleriRotation(){
+
+	$(galleriBilder[pos]).fadeOut(1000 , function(){
+
+	if(pos + 1 < galleriBilder.length)
+	{
+		++pos;
+		$(galleriBilder[pos]).fadeIn(1000);
+	}
+	else
+	{
+		pos=0;
+		$(galleriBilder[pos]).fadeIn(1000);
+	}
+	});
+
+};
+
+setInterval(galleriRotation , 5000);
+
+$(".mobil-nav-toogle").on("click", function()
+	{ 
+		$("#nav").slideToggle();
+	});
+
+
+$(".bild-galleri-container div").on("mouseover" , function(){
+	$(this).animate({bottom:"15px"});
+});
+
+$(".bild-galleri-container div").on("mouseout " , function(){
+	$(this).animate({top:"15px"});
+});
+
+});
