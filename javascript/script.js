6772140cd759fdf1e87177946e4a90bcc1f58c72
patrik <patrikyoussef@gmail.com>
2018-02-10 15:57:35 +0100

    slutversion

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,35 @@
 $(document).ready(function(){
  
-var galleriBilder = $(".bildContainer-galeri img");
-var pos = 0;
+	var galleriBilder = $(".bildContainer-galeri img");
+	var pos = 0;
 
-function galleriRotation(){
+	function galleriRotation(){
 
-	$(galleriBilder[pos]).fadeOut(1000 , function(){
+		$(galleriBilder[pos]).fadeOut(1000 , function(){
 
-	if(pos + 1 < galleriBilder.length)
-	{
-		++pos;
-		$(galleriBilder[pos]).fadeIn(1000);
-	}
-	else
-	{
-		pos=0;
-		$(galleriBilder[pos]).fadeIn(1000);
-	}
-	});
+			if(pos + 1 < galleriBilder.length){
+				++pos;
+				$(galleriBilder[pos]).fadeIn(1000);
+			}
+			else{
+				pos=0;
+				$(galleriBilder[pos]).fadeIn(1000);
+			}
+		});
+	};
 
-};
+	setInterval(galleriRotation , 5000);
 
-setInterval(galleriRotation , 5000);
-
-$(".mobil-nav-toogle").on("click", function()
-	{ 
+	$(".mobil-nav-toogle").on("click", function(){ 
 		$("#nav").slideToggle();
 	});
 
 
-$(".bild-galleri-container div").on("mouseover" , function(){
-	$(this).animate({bottom:"15px"});
-});
+	$(".bild-galleri-container div").on("mouseover" , function(){
+		$(this).animate({bottom:"15px"});
+	});
 
-$(".bild-galleri-container div").on("mouseout " , function(){
-	$(this).animate({top:"15px"});
-});
-
+	$(".bild-galleri-container div").on("mouseout " , function(){
+		$(this).animate({top:"15px"});
+	});
 });
